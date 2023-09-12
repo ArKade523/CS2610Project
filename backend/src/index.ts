6bdd2f0c1202ff3dd91fcc263ad1e47447fa9c9c
Kade Angell <ponmlk523@gmail.com>
2023-09-12 11:47:14 -0600

    Set up Express to look for frontend-web/build - Updated `npm run dev` script to build frontend-web before serving

---
+++ backend/src/index.ts
@@ -0,0 +1,15 @@
+import express, { Express, Request, Response, Application } from 'express';
+import path from 'path';
+
+const app: Application = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.resolve('../frontend-web/build')));
+
+app.get('*', (req: Request, res: Response) => {
+  res.sendFile(path.resolve('../frontend-web/build/index.html'))  
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
