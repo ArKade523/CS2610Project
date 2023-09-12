5f2dd2e16b5728c3668135e1bab04ac9c948cbfb
Kade Angell <ponmlk523@gmail.com>
2023-09-12 11:46:51 -0600

    created initial sveltekit page

---
+++ frontend-web/src/lib/components/HoverGlowBackground.svelte
@@ -0,0 +1,4 @@
+<script lang="ts">
+    // Add a glowing effect around the mouse to the background
+    
+</script>
---
+++ frontend-web/src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+
+</script>
+
+<section>
+    <h1>Page</h1>
+    <p>Page content</p>
+</section>
