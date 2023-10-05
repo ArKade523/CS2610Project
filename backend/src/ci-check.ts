a97f8d32f458bb07853b24581c8837b314d084c3
Kade Angell <ponmlk523@gmail.com>
2023-10-05 12:42:08 -0600

    Updated CI

--- backend/src/ci-check.ts
+++ backend/src/ci-check.ts
@@ -1,32 +1,48 @@
 import axios from 'axios';
 import { spawn } from 'child_process';
 
+// Initialize retry count and maximum retries
+let retries = 0;
+const maxRetries = 10;
+
+// Function to check server health
+const checkServer = async () => {
+  try {
+    const response = await axios.get('http://localhost:3000/health-check');
+    if (response.status === 200) {
+      console.log('Server is running. CI check passed.');
+      process.exit(0);  // Exit with success code
+    }
+  } catch (error) {
+    retries++;
+    if (retries < maxRetries) {
+      console.log('Retrying to connect to server...');
+      setTimeout(checkServer, 5000);  // Wait for 5 seconds before retrying
+    } else {
+      console.log('Server is not running properly.');
+      process.exit(1);  // Exit with failure code
+    }
+  }
+};
+
+// Start the server using spawn
 const server = spawn('npx', ['ts-node', './src/index.ts']);
 
+// Listen to stdout and stderr of the server process
 server.stdout.on('data', (data) => {
   console.log(`stdout: ${data}`);
+  // Assuming the server prints "Server started on port 3000" when it's ready
+  if (data.toString().includes('Server started on port 3000')) {
+    checkServer();  // Start the health check loop
+  }
 });
 
 server.stderr.on('data', (data) => {
   console.error(`stderr: ${data}`);
 });
 
-// Give the server some time to start
-setTimeout(async () => {
-  try {
-    // Check if the server is running
-    const response = await axios.get('http://localhost:3000/health-check');
-
-    if (response.status === 200) {
-      console.log('Server is running. CI check passed.');
-      process.exit(0);  // Exit with success code
-    } else {
-      console.log('Server is not running properly.');
-      process.exit(1);  // Exit with failure code
-    }
-  } catch (error) {
-    console.log('Could not make a request to the server. It is likely not running.');
-    console.error(error);
-    process.exit(1);  // Exit with failure code
-  }
-}, 5000);  // Wait for 5 seconds
+// If the server process closes, exit the script with a failure code
+server.on('close', (code) => {
+  console.log(`Server process exited with code ${code}`);
+  process.exit(1);
+});
